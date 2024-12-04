8c7b20531df764930ab9d2e72c9ea485dd49eeb5
Sergey B <bakaliarsa@vogu35.ru>
2024-12-04 13:24:25 +0300

    Страницы 133 - 143

--- urls.py
+++ urls.py
@@ -3,11 +3,14 @@ from django.urls import re_path
 from .import views
 
 urlpatterns = [
-    path('', views.index),
     re_path(r'^about', views.about),
     re_path(r'^contact', views.contact),
-    re_path(r'^products/(?P<productid>\d+)/', views.products),
-    re_path(r'products/$', views.products), # маршрут по умолчанию
-    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users),
+    # re_path(r'^products/(?P<productid>\d+)/', views.products),
+    path('products/', views.products), # маршрут по умолчанию
+    path('products/<int:productid>/', views.products),
+    # re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users),
+    path('users/', views.users), # маршрут по умолчанию
+    path('users/<int:id>/<name>/', views.users),
+    path('details/', views.details),
     path('', views.index),
 ]
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponsePermanentRedirect, HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,13 +8,19 @@ def index(request):
 def about(request):
     return HttpResponse("<h2>О сайте</h2>")
 def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
+    return HttpResponseRedirect("/about")
+    # return HttpResponse("<h2>Контакты</h2>")
 
 def products(request, productid = 1):
-    output = "<h2>Продукт № {0}</h2>".format(productid)
+    category = request.GET.get("cat", "Не задана")
+    output = "<h2>Продукт № {0} Категория: {1}</h2>".format(productid, category)
     return HttpResponse(output)
 
-def users(request, id, name):
-    output = "<h2>Пользователь</h2><h3>id:" \
-    " {0} Имя: {1}</h3>".format(id, name)
+def users(request):
+    id = request.GET.get("id", "Не задано")
+    name = request.GET.get("name", "Не задано")
+    output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3>".format(id, name)
     return HttpResponse(output)
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
